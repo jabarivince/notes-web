3e1dab5e5b813caa10b8e07a16269f51b3a15dc6
Jabari Dash <jabari.dash@gmail.com>
2019-04-20 00:42:28 -0500

    deploy everything

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-if [[ $TRAVIS_BRANCH != 'master' ]]; then
-  echo "Not deploying because this is a not the master branch"
-  exit 0
-fi
+# if [[ $TRAVIS_BRANCH != 'master' ]]; then
+#   echo "Not deploying because this is a not the master branch"
+#   exit 0
+# fi
 
 if [[ $TRAVIS_PULL_REQUEST != 'false' ]]; then
   echo "Not deploying because this is a Pull Request"
